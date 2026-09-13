23774c116700d249ebf4bd6f201e16635c637b19
Derick Dilip <derickdilip1234@gmail.com>
2026-09-13 22:26:17 +0530

    case change

--- src/data/books.ts
+++ src/data/books.ts
@@ -4,5 +4,5 @@ export interface Book {
 }
 export const books: Book[] = [
   { title: "Smart Campus Initiatives: Projects Driving Educational Automation Vol-2", isbn: "[ISBN - 978-93-7020-654-0]", publisher: "Selfypage Developers Pvt Ltd", year: "2025", coverImage: "/images/books/placeholder-2.jpg" },
-  { title: "Block chain technology for cutting edge engineering", isbn: "[ISBN]", publisher: "Scientific Iternational Publishing House",  year: "2019", coverImage: "/images/books/placeholder-1.jpg" }
+  { title: "Block Chain Technology For Cutting Edge Engineering", isbn: "[ISBN]", publisher: "Scientific Iternational Publishing House",  year: "2019", coverImage: "/images/books/placeholder-1.jpg" }
 ];
